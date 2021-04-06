235ca50693063d4882cdd1b6737f76acde6e7f3a
kxu <kevin@westsidelabs.io>
2021-04-06 19:56:09 -0400

    Removed unused code from data curation

--- generator/curate.js
+++ generator/curate.js
@@ -16,25 +16,11 @@ async function main() {
         console.log("Error reading products data", err);
     }
 
-    //const data = generateMadLibsData(products);
-    //console.log(data);
     //const data = priceVsVolume(products);
     //console.log(JSON.stringify(data));
 
     const data = priceVsWeight(products);
     console.log(JSON.stringify(data));
-
-    /*
-    const data = {
-        scatter: {
-            ...weightVsVolume(products),
-            ...createScatterData(products),
-        },
-        ...generateHistogramData(products),
-    };
-    console.log(JSON.stringify(data, null, 2));
-    console.log(JSON.stringify(data));
-    */
 }
 
 async function loadProductsData(url) {
@@ -53,65 +39,83 @@ async function loadProductsData(url) {
     return products;
 }
 
+
 /************************************************
- * Rankings by Property (weight, volume, price)
- *  for use in Madlibs
+ * Scatter Plots
  ***********************************************/
 
-function generateMadLibsData(products) {
-
-    // store minimal product data in array
+function priceVsVolume(products) {
     var data = [];
     for (const product of products) {
-        const len = product.weight.data.length;
-        const last = len - 1;
+        for (const variant of product.volume.data) {
+            const name = (variant.key === 'One Size') ?
+                product.name :
+                `${product.name} - ${variant.key}`;
 
-        console.log(product);
-        const p = {
-            sku: product.sku,
-            price: parseFloat(product.price),
-
-            best_used_for: product.best_used_for,
-            weight_low:  _.get(product, 'weight.data[0].val', null),
-            weight_high: _.get(product, `weight.data[${last}].val`, null),
-            volume_low:  _.get(product, 'volume.data[0].val', null),
-            volume_high: _.get(product, `volume.data[${last}].val`, null),
-        }
-        data.push(p);
-    }
-    return { rankings: data };
-
-    /*
-    const data = {
-        count: 64,
-        1234: {
-            weight: {
-                rank: 4,
-                rank_adj: 7,
-            },
-            volume: {
-                rank: 
-                rank_adj: 
-            }
-
-            rank_weight: 2,
-            rank_price: 3,
-            rank_volume: 15,
+            data.push({
+                x: variant.val,
+                y: product.price,
+                name: name,
+            });
         }
     }
-    */
+    return data;
 }
 
-// sort by object property
+// iterate over all files in ./data/products
+// fetch the weight field
+// aggregate them into a separate json object
+function priceVsWeight(products) {
+    var data = [];
+    for (const product of products) {
+        for (const variant of product.weight.data) {
+            const name = (variant.key === 'One Size') ?
+                product.name :
+                `${product.name} - ${variant.key}`;
 
+            const lbs = convert(variant.val).from('oz').to('lb');
+            data.push({
+                x: Math.round(lbs * 100) / 100,
+                y: product.price,
+                name: name,
+            });
+        }
+    }
+    return data;
+}
+
+// currently unused
+function weightVsVolume(products) {
+    var data = [];
+    for (const product of products) {
+        for (var i = 0; i < product.weight.data.length; i++) {
+            if (!product.volume.data[i] || !product.weight.data[i]) {
+                break;
+            }
+            const weight = product.weight.data[i].val;
+            const volume = product.volume.data[i].val;
+
+            const name = (product.weight.data[i].key === 'One Size') ?
+                product.name :
+                `${product.name} - ${product.weight.data[i].key}`;
+
+            const lbs = convert(weight).from('oz').to('lb');
+            data.push({
+                x: weight,
+                y: Math.round(lbs * 100) / 100,
+                name: name,
+            });
+        }
+    }
+    return { weight_vs_volume: data };
+}
 
 /************************************************
  * Histogram
  ***********************************************/
 
-/*
-[
-  { count: 0, low: 70, high: 102, data: [] },       // 0
+/**
+ [{ count: 0, low: 70, high: 102, data: [] },       // 0
   { count: 0, low: 102, high: 134, data: [] },      // 1
   { count: 0, low: 134, high: 166, data: [] },      // 2
   { count: 0, low: 166, high: 198, data: [] },      // 3
@@ -120,8 +124,7 @@ function generateMadLibsData(products) {
   { count: 0, low: 262, high: 294, data: [] },      // 6
   { count: 0, low: 294, high: 326, data: [] },      // 7
   { count: 0, low: 326, high: 358, data: [] },      // 8
-  { count: 0, low: 358, high: 390, data: [] }       // 9
-]
+  { count: 0, low: 358, high: 390, data: [] }]      // 9
 */
 function generateHistogramData(products) {
     const NUM_BUCKETS = 10;
@@ -175,78 +178,3 @@ function generateHistogramData(products) {
 
     return { histogram: stats };
 }
-
-
-/************************************************
- * Scatter Plots
- ***********************************************/
-
-function priceVsVolume(products) {
-    var data = [];
-    for (const product of products) {
-        for (const variant of product.volume.data) {
-            const name = (variant.key === 'One Size') ?
-                product.name :
-                `${product.name} - ${variant.key}`;
-
-            data.push({
-                x: variant.val,
-                y: product.price,
-                name: name,
-            });
-        }
-    }
-    return data;
-}
-
-// iterate over all files in ./data/products
-// fetch the weight field
-// aggregate them into a separate json object
-function priceVsWeight(products) {
-    var data = [];
-    for (const product of products) {
-        for (const variant of product.weight.data) {
-            const name = (variant.key === 'One Size') ?
-                product.name :
-                `${product.name} - ${variant.key}`;
-
-            const lbs = convert(variant.val).from('oz').to('lb');
-            data.push({
-                y: product.price,
-                x: Math.round(lbs * 100) / 100,
-                name: name,
-            });
-        }
-    }
-    return data;
-}
-
-function weightVsVolume(products) {
-    var data = [];
-    for (const product of products) {
-        for (var i = 0; i < product.weight.data.length; i++) {
-            if (!product.volume.data[i] || !product.weight.data[i]) {
-                break;
-            }
-            const weight = product.weight.data[i].val;
-            const volume = product.volume.data[i].val;
-
-            const name = (product.weight.data[i].key === 'One Size') ?
-                product.name :
-                `${product.name} - ${product.weight.data[i].key}`;
-
-            const lbs = convert(weight).from('oz').to('lb');
-            data.push({
-                x: weight,
-                y: Math.round(lbs * 100) / 100,
-                name: name,
-            });
-
-            //break;
-        }
-    }
-
-    return { weight_vs_volume: data };
-}
-
-
